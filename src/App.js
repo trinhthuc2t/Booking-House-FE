9fb730de347e3849c7353ab315a2992d748a3c77
tuuyen09 <125869542+tuuyen09@users.noreply.github.com>
2023-10-04 15:00:09 +0700

    register

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import {Route, Routes} from "react-router-dom";
+import Register from "./login-register/register";
 
 function App() {
-  return (
-    <div className="App">
-      Hello
-        <Routes>
-            <Route/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path={"/register"} element={<Register/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
