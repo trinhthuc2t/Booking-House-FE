b74f368b3666ee06216064eacbc606a322dcc489
trinhthuc2t <thuctrinh.2t@gmail.com>
2023-10-04 11:35:11 +0700

    pull dev

--- src/service/houseByIdService.js
+++ src/service/houseByIdService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HouseService = {
+const HouseByIdService = {
 
 
     getHousesByOwnerId: (ownerId) => {
@@ -41,46 +41,6 @@ const HouseService = {
         })
     },
 
-
-
-    deleteProduct: (id) => {
-        return new Promise((resolve, reject) => {
-            axios
-                .delete(`https://dummyjson.com/products/${id}`)
-                .then((response) => {
-                    resolve(response.data);
-                })
-                .catch((err) => {
-                    reject(err);
-                });
-        });
-    },
-
-
-    save: (product) => {
-        return new Promise((resolve, reject) => {
-            axios.post('https://dummyjson.com/products/add', product)
-                .then(response => {
-                    resolve(response);
-                })
-                .catch(function (err) {
-                    reject(err)
-                })
-        })
-    },
-
-    getProduct: (id)=>{
-
-        return new Promise((resolve,reject) =>
-            axios.get('https://dummyjson.com/products/'+id)
-                .then(res =>{
-                    resolve(res.data);
-                })
-                .catch((err)=> {
-                    reject(err)
-                })
-        )
-    }
 
 
 };
