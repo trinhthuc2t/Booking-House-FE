57762af4bb1f4f8065826cc7b8e71e4f24aa79e0
hieu16129 <infor.duchieu.1999@gmail.com>
2023-10-04 10:15:13 +0700

    edit profile update

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import { ToastContainer } from 'react-toastify';
 
 import 'react-toastify/dist/ReactToastify.css';
 import React from "react";
+import {Route, Routes} from "react-router-dom";
+import EditProfile from "./component/account/crud/EditProfile";
 
 
 function App() {
   return (
     <div className="App">
-       <Home></Home>
+        <Routes>
+            <Route path={"/editProfile/:id"} element={<EditProfile/>}/>
+        </Routes>
         <ToastContainer/>
     </div>
   );
