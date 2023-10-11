3f7888bd2983295572a538dfc8b9e8a619671347
tuuyen09 <125869542+tuuyen09@users.noreply.github.com>
2023-10-11 17:15:31 +0700

    booking

--- src/components/Booking/ListBooking.js
+++ src/components/Booking/ListBooking.js
@@ -1,9 +1,13 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {Link} from "react-router-dom";
+import _ from "lodash";
+import {formatCurrency} from "../../service/format";
+import {Pagination} from "@mui/material";
 
 function ListBooking() {
     const [bookings, setBookings] = useState([])
+    const [totalPages, changePage] = useState(0)
     useEffect(() => {
         axios.get("http://localhost:8080/api/bookings/list")
             .then(res => {
@@ -14,45 +18,111 @@ function ListBooking() {
                 console.log(err)
             })
     }, [])
+
+    const deleteBooking = (id) => {
+        axios.post("http://localhost:8080/api/bookings/delete/" + id)
+            .then((res) => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
     return (
-        <>
-            {bookings.map(b => {
-                return (
-                    <div className="container">
-                        <h4>Lịch đặt thuê</h4>
-                        <table>
-                            <thead>
-                            <tr>
-                                <th>Thời gian thuê</th>
-                                <th>Tên ngôi nhà</th>
-                                <th>Tên khách hàng</th>
-                                <th>Tổng đơn</th>
-                                <th>Trạng thái đơn</th>
-                                <th>Hành động</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {bookings.map(booking => (
-                                <tr key={booking.id}>
-                                    <td>{booking.startTime}</td>
-                                    <td>{booking.house.name}</td>
-                                    <td>{booking.account.firstname + " " + booking.account.lastname}</td>
-                                    <td>{booking.total}</td>
-                                    <td>{booking.status}</td>
+        <div className="col-9">
+            <h3 className="text-uppercase text-center mb-5">Lịch đặt thuê</h3>
+            <div className="mb-3 py-4 px-3"
+                 style={{backgroundColor: "rgb(0,185,142)"}}>
+                <table className="table">
+                    <thead>
+                    <tr align="center" style={{fontSize: '20px'}}>
+                        <th>STT</th>
+                        <th>Nhà</th>
+                        <th style={{minWidth: '130px'}}>Lịch thuê</th>
+                        <th style={{width: '150px'}}>Trạng thái</th>
+                        <th style={{width: '150px'}}>Hành động</th>
+                    </tr>
+                    </thead>
+                    <tbody style={{verticalAlign: 'middle'}}>
+                    {!_.isEmpty(bookings) ? bookings.map((booking, index) => {
+                            return (
+                                <tr key={booking.id} align="center">
                                     <td>
-                                       <Link to={"/booking/checkin/" + booking.id}><button>Check in</button></Link>
+                                        <h5>{index + 1}</h5>
                                     </td>
                                     <td>
-                                        <Link to={"/booking/checkout/" + booking.id}><button>Check out</button></Link>
+                                        <div className="nav-link d-flex align-items-center">
+                                            <img className="flex-shrink-0 img-fluid border rounded"
+                                                 src={booking.thumbnail} alt=""
+                                                 style={{width: 80, height: 80}}/>
+                                            <div className="d-flex flex-column text-start ps-4">
+                                                <h5 className="text-truncate">{booking.house.name}</h5>
+                                                <div className="text-truncate me-3">
+                                                    {booking.account.firstname + booking.account.lastname}
+                                                </div>
+                                                <div className="text-truncate">
+                                                    {formatCurrency(booking.total)}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+
+                                    <td className="mb-3">
+                                        <div>
+                                            <p>Bắt đầu: {booking.startTime}</p>
+                                            <p>Kết thúc: {booking.endTime}</p>
+                                        </div>
+                                    </td>
+                                    <td className="mb-3">
+                                        {booking.status}
+                                    </td>
+
+                                    <td className="mb-3">
+                                        {booking.status === "Chờ nhận phòng" && (
+                                            <Link to={"/booking/checkin/" + booking.id}>
+                                                <button className="btn"
+                                                        style={{backgroundColor: "rgb(0,185,142)", color: "white"}}>Check in
+                                                </button>
+                                            </Link>
+                                        )}
+                                        {booking.status === "Đang ở" && (
+                                            <Link to={"/booking/checkout/" + booking.id}>
+                                                <button className="btn"
+                                                        style={{backgroundColor: "rgb(0,185,142)", color: "white"}}>Check
+                                                    out
+                                                </button>
+                                            </Link>
+                                        )}
+                                        {booking.status === "Đã trả phòng" && (
+                                            <button onClick={() => {
+                                                deleteBooking(booking.id)
+                                            }} className="btn"
+                                                    style={{backgroundColor: "rgb(0,185,142)", color: "white"}}>Xoá</button>
+                                        )}
                                     </td>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                            )
+                        })
+                        :
+                        <tr align="center">
+                            <td colSpan="5" className="pt-3 fs-5 text-danger">Danh sách trống</td>
+                        </tr>
+                    }
+                    </tbody>
+                </table>
+                {!_.isEmpty(bookings) ?
+                    <div className="col-12 mt-5 d-flex justify-content-center">
+                        <Pagination count={totalPages} size="large" variant="outlined" shape="rounded"
+                                    onChange={changePage} color="primary"/>
                     </div>
-                )
-            })}
-        </>)
+                    :
+                    null
+                }
+            </div>
+        </div>
+
+    )
 }
 
 
