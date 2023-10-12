ecd683977f459615dbcaf92e707548f691bed887
Hao <nguyenduchao95@gmail.com>
2023-10-12 18:19:26 +0700

    complete booking

--- src/service/BookingService.js
+++ src/service/BookingService.js
@@ -1,15 +1,33 @@
-import React from 'react';
 import instance from "./axiosConfig";
+import axios from "axios";
 
 const BookingService = {
-    getHistoryByAccount : async (id , currentPage = 0) => {
-        return await instance.get(`/api/bookings/getByIdAccount/${id}?page=${currentPage}&`  );
+    getHistoryByAccount: async (id, currentPage = 0) => {
+        return await instance.get(`/api/bookings/getByIdAccount/${id}?page=${currentPage}&`);
     },
-    cancelBooking : async (idBooking) => {
+    cancelBooking: async (idBooking) => {
         return await instance.get("/api/bookings/cancelBooking/" + idBooking);
+    },
+    getBookingsByOwnerWeek: (ownerId, month, year, startDay, endDay) => {
+        return new Promise((resolve, reject) => {
+            axios
+                .get(`http://localhost:8080/api/bookings/${ownerId}/week?month=${month}&year=${year}&startDay=${startDay}&endDay=${endDay}`)
+                .then(response => {
+                    resolve(response.data);
+                    console.log(response.data)
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        });
+    },
+    getBookingsByHouseId: (houseId) => {
+        return instance.get(`/api/bookings/house/${houseId}`);
+    },
+
+    bookingHouse: (booking) => {
+        return instance.post('/api/bookings', booking);
     }
-
-
 };
 
 export default BookingService;
